34bb7c310bdbe0516f07ae9265d4befe866a28b1
annalla <danghongxuyen2000@gmail.com>
2021-12-11 14:12:11 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Button, Space,Progress, Input } from 'antd';
+import './App.less';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Space direction='vertical' size={30}>
+    <Button type="primary">Button</Button>
+    <Input placeholder='type here'></Input>
+    <Progress percent={50} type='circle'></Progress>
+    </Space>
+  </div>
+);
 
 export default App;
